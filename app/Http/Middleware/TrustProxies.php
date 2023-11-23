d877583a622d1f5fb93640dd94a8425868617a09
zakki-2338 <barubatosuto@gmail.com>
2023-11-23 02:20:19 +0000

    Switch database from MySQL to Supabase
    Deploy application on Fly.io

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,7 @@ class TrustProxies extends Middleware
      *
      * @var array<int, string>|string|null
      */
-    protected $proxies = '*'; // 全プロキシを信用する設定に変更
+    protected $proxies;
 
     /**
      * The headers that should be used to detect proxies.
